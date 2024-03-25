9e19e2a5215cd0d99e76dc1c2ebc5cbd4ac48a48
bernal4 <160553760+bernal4@users.noreply.github.com>
2024-03-24 20:42:21 -0500

    Add files via upload

---
+++ operadores.js/ejercicio2.js
@@ -0,0 +1,16 @@
+// entradas
+
+console.log ("2. suma de dos numeros del teclado: ");
+console.log ("ingrese el primer numero:");
+let primero = 8;
+console.log ("ingrese el segundo numero");
+let segundo = 15;
+
+
+//proceso
+let resultado = (primero + segundo)
+
+
+
+//salida
+console.log(resultado);
---
+++ operadores.js/ejercicio3.js
@@ -0,0 +1,15 @@
+// entradas
+
+console.log ("3.euros a dolares: ");
+console.log ("ingrese la cantidad de euros:");
+let euros = 5;
+let dolares
+
+
+//proceso
+let resultado = (dolares * euros) * 1.2;
+
+
+
+//salida
+console.log(resultado);
