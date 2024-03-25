91cdd4c3d70eed32af36829faa511d26d6f4c6ec
bernal4 <160553760+bernal4@users.noreply.github.com>
2024-03-24 20:41:52 -0500

    Add files via upload

---
+++ condicionales.js/condicional4.js
@@ -0,0 +1,20 @@
+console.log("Escriba dos numeros")
+
+let a = 65
+let b = 8
+
+console.log(a)
+console.log(b)
+
+if( a < b){
+    let resultado = a + b
+    console.log("El resultado es: ")
+    console.log(resultado)
+
+}else{
+    if(a > b){
+    let resultado = a - b
+    console.log("El resultado es: ")
+    console.log(resultado)
+ }
+}
